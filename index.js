60b6d805dea41d30efe1ee9fbefaebe63fd725f5
CaptainBadasXT <kyjuanjulien76@gmail.com>
2026-02-27 12:44:55 -0400

    mn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];// 10 elements
-
-for(let i=0; i< arr.length; i++){
-  console.log(arr[i])
+function printDate(){
+   console.log(Date().toLocaleTimeString());
 }
+      
+setInterval(printDate, 1000);

7b635f79a1f5d1ae300e5fde90251b0ed9f391c1
CaptainBadasXT <kyjuanjulien76@gmail.com>
2026-03-09 18:19:37 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-function printDate(){
-   console.log(Date().toLocaleTimeString());
-}
-      
-setInterval(printDate, 1000);
+let arr = []//empty array
+let arr2 = [1, 2, 3, 4, 5, 6, 7]//initialize with elements
+let arr3 = [1, 12.9, "Hi", '🎈', ]// heterogeneous array
+
+console.log(arr[0]);// 1
+console.log(arr3[3]);//🎈
